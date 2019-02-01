6a6d8155e660290b7fb9160bdd47bd0c56b90da2
nvnvyezi <1187128658@qq.com>
2019-02-01 15:26:24 +0800

    feat: 添加boxes

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { request } = require('./utils/request')
+
 //app.js
 App({
   onLaunch: function () {
@@ -18,27 +20,27 @@ App({
             'content-type': 'json'
           },
           success: res => {
-            console.log(res)
             const { openid, session_key } = res.data
             this.globalData.openId = openid
             this.globalData.sessionKey = session_key
-            wx.request({
-              url: `http://127.0.0.1:8000/homework/findUser`,
+
+            const data = {
+              url: `/findUser`,
+              method: 'get',
               data: {
                 userId: openid,
-              },
-              method: 'POST',
-              dataType: 'json',
-              success: res => {
-                console.log(res)
-                const { data } = res
+              }
+            }
+            request(data, (bool, res) => {
+              if (bool) {
                 if (data.status === 200) {
+                  const { name: nickName, portrait: avatarUrl} = res
                   this.globalData.userInfo = {
-
+                    nickName,
+                    avatarUrl,
                   }
                 }
-              },
-              fail: err => {
+              } else {
                 wx.showToast({
                   title: '登录失败',
                   icon: 'none',
@@ -57,10 +59,8 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
-              console.log(res)
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
---
+++ pages/boxes/index.js
@@ -0,0 +1,64 @@
+//index.js
+//获取应用实例
+const { request } = require('../../utils/request')
+
+const app = getApp()
+
+Page({
+  data: {
+    title: '作业盒子',
+    msg: '',
+    msg1: '',
+  },
+  //获取加入的盒子
+  getAddBoxes: function() {
+    request({
+      url: '/getAddBoxes',
+      method: 'GET',
+      data: {
+        userId: app.globalData.openId,
+      }
+    }, (bool, res) => {
+      if (bool) {
+        this.setData({
+          msg1: res.msg,
+        })
+      } else {
+        wx.showToast({
+          title: '获取失败',
+          icon: 'none',
+          duration: 500,
+        })
+      }
+    })
+  },
+  // 获取自己创建的盒子
+  getCreateBoxes: function() {
+    request({
+      url: '/getBoxes',
+      data: {
+        createId: app.globalData.openId,
+      },
+      method: 'GET',
+    }, (bool, res) => {
+      if (bool) {
+        this.setData({
+          msg: res.msg,
+        })
+      } else {
+        wx.showToast({
+          title: '登录失败',
+          icon: 'none',
+          duration: 500,
+        })
+      }
+    })
+  },
+  //创建盒子
+  createBox: function () {
+    // request()
+  },
+  onLoad: function () {
+    this.getCreateBoxes()
+  },
+})
